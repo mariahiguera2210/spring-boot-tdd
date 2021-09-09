1c772c9c0d830da16e7cbd6cda40ccfad0fe12d9
sancarbar <sancarbar@gmail.com>
2021-09-08 22:24:46 -0500

    Initial commit.

---
+++ src/test/java/org/adaschool/tdd/TddApplicationTests.java
@@ -0,0 +1,15 @@
+package org.adaschool.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
